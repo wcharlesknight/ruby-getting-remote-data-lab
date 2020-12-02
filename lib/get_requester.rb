1dcffb5add1a15d466a5981df6cc35b4fbc04bb8
Charlie Knight <williamchknight@gmail.com>
2020-12-02 14:21:19 -0800

    done

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+require "net/http"
+require "open-uri"
+require "json" 
+
+
+class GetRequester
+
+    def initialize(url)
+        @url = url 
+    end 
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+    
+    def parse_json
+        programs = JSON.parse(self.get_response_body)
+        programs.collect do |program|
+            program
+        end
+    end 
+
+end 
